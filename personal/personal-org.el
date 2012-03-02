a5ed7c650bf7e4a4ccc37e73fe5aa8ee4a0a93d7
Perry Kundert <perry@kundert.ca>
2012-03-02 07:38:57 -0700

    Further org-mode config; incl. download from git repo

--- personal/personal-org.el
+++ personal/personal-org.el
@@ -1,3 +1,11 @@
+;; org-mode setup
+;;
+;;     Much of the configuration here comes from:
+;;
+;;         http://doc.norang.ca/org-mode.html
+;;
+
+
 ;; In addition to the default org bindings:
 ;; C-c a           org-agenda
 ;; C-c b           org-iswitchb
@@ -22,8 +30,28 @@
 (define-key key-translation-map (kbd "ESC [ S R")   (kbd "<S-return>"))
 (define-key key-translation-map (kbd "ESC [ C S R") (kbd "<C-S-return>"))
 
+;; Paths and file types.  We use our own local copy of org-mode,
+;; because the one included with emacs 24 seems to often be broken.
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/personal/org-mode/lisp"))
+(require 'org-install)
+
+(add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
+
 (setq org-agenda-files (quote ("~/org/")))
 (setq org-default-notes-file "~/org/refile.org")
+
+;; flyspell mode for spell checking everywhere
+(add-hook 'org-mode-hook 'turn-on-flyspell 'append)
+
+;; Disable C-c [ and C-c ] in org-mode
+(add-hook 'org-mode-hook
+          (lambda ()
+            ;; Undefine C-c [ and C-c ] since this breaks my
+            ;; org-agenda files when directories are include It
+            ;; expands the files in the directories individually
+            (org-defkey org-mode-map "\C-c["    'undefined)
+            (org-defkey org-mode-map "\C-c]"    'undefined))
+          'append)
 
 ;; MobileOrg iPhone app support
 ;; Set to the location of your Org files on your local system
@@ -33,24 +61,82 @@
 ;; Set to <your Dropbox root directory>/MobileOrg.
 (setq org-mobile-directory "~/Dropbox/MobileOrg")
 
+;; org-mode states, colors
+(setq org-todo-keywords
+      (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!/!)")
+              (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "PHONE"))))
+
+(setq org-todo-keyword-faces
+      (quote (("TODO" :foreground "red" :weight bold)
+              ("NEXT" :foreground "blue" :weight bold)
+              ("DONE" :foreground "forest green" :weight bold)
+              ("WAITING" :foreground "orange" :weight bold)
+              ("HOLD" :foreground "magenta" :weight bold)
+              ("CANCELLED" :foreground "forest green" :weight bold)
+              ("PHONE" :foreground "forest green" :weight bold))))
+
+(setq org-use-fast-todo-selection t)
+
+(setq org-todo-state-tags-triggers
+      (quote (("CANCELLED" ("CANCELLED" . t))
+              ("WAITING" ("WAITING" . t))
+              ("HOLD" ("WAITING" . t) ("HOLD" . t))
+              (done ("WAITING") ("HOLD"))
+              ("TODO" ("WAITING") ("CANCELLED") ("HOLD"))
+              ("NEXT" ("WAITING") ("CANCELLED") ("HOLD"))
+              ("DONE" ("WAITING") ("CANCELLED") ("HOLD")))))
+
+;; Tags with fast selection keys
+;; @... tags are mutually exclusive, and indicate that the item must
+;; actual occur @ the target location.  The other tags simply indicate
+;; that the item is related to the concept.
+(setq org-tag-alist (quote ((:startgroup)
+                            ("@enbridge" . ?e)
+                            ("@home" . ?H)
+                            ("@farm" . ?f)
+                            (:endgroup)
+                            ("PHONE" . ?p)
+                            ("WAITING" . ?w)
+                            ("HOLD" . ?h)
+                            ("PERSONAL" . ?P)
+                            ("WORK" . ?W)
+                            ("ENBRIDGE" . ?E)
+                            ("FARM" . ?F)
+                            ("MARK" . ?M)
+                            ("NOTE" . ?n)
+                            ("CANCELLED" . ?c)
+                            ("FLAGGED" . ??))))
+
+;; Allow setting single tags without the menu
+(setq org-fast-tag-selection-single-key (quote expert))
+
+;; For tag searches ignore tasks with scheduled and deadline dates
+(setq org-agenda-tags-todo-honor-ignore-options t)
+
 ;; org-capture templates
 ;; Capture templates for: TODO tasks, Notes, appointments, phone calls, and org-protocol
 (setq org-capture-templates
-      (quote (("t" "todo" entry (file "~/Dropbox/org/refile.org")
+      (quote (("t" "todo" entry (file "~/org/refile.org")
                "* TODO %?\n%U\n%a\n  %i" :clock-in t :clock-resume t)
-              ("n" "note" entry (file "~/Dropbox/org/refile.org")
+              ("n" "note" entry (file "~/org/refile.org")
                "* %? :NOTE:\n%U\n%a\n  %i" :clock-in t :clock-resume t)
-              ("j" "Journal" entry (file+datetree "~/Dropbox/org/diary.org")
+              ("j" "Journal" entry (file+datetree "~/org/diary.org")
                "* %?\n%U\n  %i" :clock-in t :clock-resume t)
-              ("w" "org-protocol" entry (file "~/Dropbox/org/refile.org")
+              ("w" "org-protocol" entry (file "~/org/refile.org")
                "* TODO Review %c\n%U\n  %i" :immediate-finish t)
-              ("p" "Phone call" entry (file "~/Dropbox/org/refile.org")
+              ("p" "Phone call" entry (file "~/org/refile.org")
                "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t))))
 
 ;; org-refile settings
 ;; Targets include this file and any file contributing to the agenda - up to 9 levels deep
 (setq org-refile-targets (quote ((nil :maxlevel . 9)
                                  (org-agenda-files :maxlevel . 9))))
+
+;; Stop using paths for refile targets - we file directly with IDO
+(setq org-refile-use-outline-path nil)
+
+;; Targets complete directly with IDO
+(setq org-outline-path-complete-in-steps nil)
 
 ;; Allow refile to create parent tasks with confirmation
 (setq org-refile-allow-creating-parent-nodes (quote confirm))
@@ -61,3 +147,14 @@
   (not (member (nth 2 (org-heading-components)) org-done-keywords)))
 
 (setq org-refile-target-verify-function 'bh/verify-refile-target)
+
+
+;; Task Estimates
+;; Set default column view headings: Task Effort Clock_Summary
+(setq org-columns-default-format "%80ITEM(Task) %10Effort(Effort){:} %10CLOCKSUM")
+
+;; global Effort estimate values
+;; global STYLE property values for completion
+(setq org-global-properties
+      (quote (("Effort_ALL" . "0:15 0:30 0:45 1:00 2:00 3:00 4:00 5:00 6:00 0:00")
+              ("STYLE_ALL" . "habit"))))
